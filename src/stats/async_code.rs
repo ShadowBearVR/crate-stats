b331a92126672dfe3aa1bdef37c985dc943459f6
Sam Sartor <me@samsartor.com>
2022-12-06 22:46:44 -0500

    first line and last line

--- src/stats/async_code.rs
+++ src/stats/async_code.rs
@@ -34,6 +34,7 @@ impl Visit<'_> for Stats<'_, '_> {
         let mut child = Stats {
             log: self.log.fork(),
             count: 0,
+            outermost: false,
         };
         visit::visit_item_fn(&mut child, node);
 
@@ -44,35 +45,65 @@ impl Visit<'_> for Stats<'_, '_> {
         let count = child.count;
 
         self.log.db.execute(
-            r"INSERT INTO async_code (async_code_type, block_count, file_name, line_number, version_id) VALUES ($1, $2, $3, $4, $5)",
-            &[&AsyncCodeType::Function, &(count as i32), &self.log.file_name, &(node.sig.span().start().line as i32), &self.log.version_id],
+            r"INSERT INTO async_code (async_code_type, block_count, file_name, first_line_number, last_line_number, outermost, version_id)
+            VALUES                   ($1,              $2,          $3,        $4,                $5,               $6,        $7)",
+            &[
+                &AsyncCodeType::Function,
+                &(count as i32),
+                &self.log.file_name,
+                &(node.span().start().line as i32),
+                &(node.span().end().line as i32),
+                &self.outermost,
+                &self.log.version_id],
         ).unwrap();
 
-        trace!(count = count, ty = "Function");
+        trace!(count = count, ty = "Function", outermost = self.outermost);
     }
 
     fn visit_expr_async(&mut self, node: &syn::ExprAsync) {
-        visit::visit_expr_async(self, node);
-
+        let mut child = Stats {
+            log: self.log.fork(),
+            count: 0,
+            outermost: false,
+        };
+        visit::visit_expr_async(&mut child, node);
+        self.count += child.count;
         self.count += 1;
 
         self.log.db.execute(
-            r"INSERT INTO async_code (async_code_type, block_count, file_name, line_number, version_id) VALUES ($1, $2, $3, $4, $5)",
-            &[&AsyncCodeType::Block, &None::<i32>, &self.log.file_name, &(node.span().start().line as i32), &self.log.version_id],
-
+            r"INSERT INTO async_code (async_code_type, block_count, file_name, first_line_number, last_line_number, outermost, version_id)
+            VALUES                   ($1,              $2,          $3,        $4,                $5,               $6,        $7)",
+            &[
+                &AsyncCodeType::Block,
+                &None::<i32>,
+                &self.log.file_name,
+                &(node.span().start().line as i32),
+                &(node.span().end().line as i32),
+                &self.outermost,
+                &self.log.version_id],
         ).unwrap();
 
-        trace!(ty = "Block");
+        trace!(ty = "Block", outermost = self.outermost);
     }
 }
 
 struct Stats<'log, 'db> {
     log: super::Logger<'log, 'db>,
     count: usize,
+    outermost: bool,
 }
 
 pub const RUNNER: super::Runner = super::Runner {
-    collect: |file, log| visit::visit_file(&mut Stats { log, count: 0 }, file),
+    collect: |file, log| {
+        visit::visit_file(
+            &mut Stats {
+                log,
+                count: 0,
+                outermost: true,
+            },
+            file,
+        )
+    },
     init: |db| {
         AsyncCodeType::init(db);
         db.batch_execute(
@@ -80,7 +111,9 @@ pub const RUNNER: super::Runner = super::Runner {
             CREATE TABLE async_code (
                 async_code_type "AsyncCodeType",
                 block_count INT,
-                line_number INT,
+                first_line_number INT,
+                last_line_number INT,
+                outermost BOOL,
                 file_name TEXT,
                 version_id UUID references versions(id)
             );
@@ -95,6 +128,7 @@ pub const RUNNER: super::Runner = super::Runner {
 #[traced_test]
 fn test_async_fn() {
     RUNNER.collect_mock("async_fn");
-    assert!(logs_contain(r#"ty="Block""#));
-    assert!(logs_contain(r#"count=4 ty="Function""#));
+    assert!(logs_contain(r#"ty="Block" outermost=false"#));
+    assert!(!logs_contain(r#"ty="Block" outermost=true"#));
+    assert!(logs_contain(r#"count=4 ty="Function" outermost=true"#));
 }
--- src/stats/unsafe_code.rs
+++ src/stats/unsafe_code.rs
@@ -34,6 +34,7 @@ impl Visit<'_> for Stats<'_, '_> {
         let mut child = Stats {
             log: self.log.fork(),
             count: 0,
+            outermost: false,
         };
         visit::visit_item_fn(&mut child, node);
 
@@ -44,28 +45,55 @@ impl Visit<'_> for Stats<'_, '_> {
         let count = child.count;
 
         self.log.db.execute(
-            r"INSERT INTO unsafe_code (unsafe_code_type, block_count, file_name, version_id) VALUES ($1, $2, $3, $4)",
-            &[&UnsafeCodeType::Function, &(count as i32), &self.log.file_name, &self.log.version_id],
+            r"INSERT INTO unsafe_code (unsafe_code_type, block_count, file_name, first_line_number, last_line_number, outermost, version_id)
+            VALUES                    ($1,               $2,          $3,        $4,                $5,               $6,        $7)",
+            &[
+                &UnsafeCodeType::Function,
+                &(count as i32),
+                &self.log.file_name,
+                &(node.span().start().line as i32),
+                &(node.span().end().line as i32),
+                &self.outermost,
+                &self.log.version_id],
         ).unwrap();
 
-        trace!(count = count, ty = "Function");
+        trace!(count = count, ty = "Function", outermost = self.outermost);
     }
 
     fn visit_expr_unsafe(&mut self, node: &syn::ExprUnsafe) {
-        visit::visit_expr_unsafe(self, node);
-
+        let mut child = Stats {
+            log: self.log.fork(),
+            count: 0,
+            outermost: false,
+        };
+        visit::visit_expr_unsafe(&mut child, node);
+        self.count += child.count;
         self.count += 1;
 
         self.log.db.execute(
-            r"INSERT INTO unsafe_code (unsafe_code_type, block_count, file_name, line_number, version_id) VALUES ($1, $2, $3, $4, $5)",
-            &[&UnsafeCodeType::Block, &None::<i32>, &self.log.file_name, &(node.span().start().line as i32), &self.log.version_id],
+            r"INSERT INTO unsafe_code (unsafe_code_type, block_count, file_name, first_line_number, last_line_number, outermost, version_id)
+            VALUES                    ($1,               $2,          $3,        $4,                $5,               $6,        $7)",
+            &[
+                &UnsafeCodeType::Block,
+                &None::<i32>,
+                &self.log.file_name,
+                &(node.span().start().line as i32),
+                &(node.span().end().line as i32),
+                &self.outermost,
+                &self.log.version_id],
         ).unwrap();
 
-        trace!(ty = "Block");
+        trace!(ty = "Block", outermost = self.outermost);
     }
 
     fn visit_expr_call(&mut self, node: &syn::ExprCall) {
-        visit::visit_expr_call(self, node);
+        let mut child = Stats {
+            log: self.log.fork(),
+            count: 0,
+            outermost: false,
+        };
+        visit::visit_expr_call(&mut child, node);
+        self.count += child.count;
 
         let syn::Expr::Path(path) = &*node.func else {
             return
@@ -100,10 +128,20 @@ impl Visit<'_> for Stats<'_, '_> {
 struct Stats<'log, 'db> {
     log: super::Logger<'log, 'db>,
     count: usize,
+    outermost: bool,
 }
 
 pub const RUNNER: super::Runner = super::Runner {
-    collect: |file, log| visit::visit_file(&mut Stats { log, count: 0 }, file),
+    collect: |file, log| {
+        visit::visit_file(
+            &mut Stats {
+                log,
+                count: 0,
+                outermost: true,
+            },
+            file,
+        )
+    },
     init: |db| {
         UnsafeCodeType::init(db);
         db.batch_execute(
@@ -112,7 +150,9 @@ pub const RUNNER: super::Runner = super::Runner {
                 unsafe_code_type "UnsafeCodeType",
                 block_count INT,
                 file_name TEXT,
-                line_number INT,
+                first_line_number INT,
+                last_line_number INT,
+                outermost BOOL,
                 version_id UUID references versions(id)
             );
             CREATE INDEX usafe_code_version_index ON unsafe_code(version_id);
@@ -134,8 +174,8 @@ pub const RUNNER: super::Runner = super::Runner {
 #[traced_test]
 fn test_unsafe_fn() {
     RUNNER.collect_mock("unsafe_fn");
-    assert!(logs_contain(r#"ty="Block""#));
-    assert!(logs_contain(r#"count=4 ty="Function""#));
+    assert!(logs_contain(r#"ty="Block" outermost=false"#));
+    assert!(logs_contain(r#"count=4 ty="Function" outermost=true"#));
 }
 
 #[test]
